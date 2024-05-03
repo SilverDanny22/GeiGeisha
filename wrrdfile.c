aa52ccfc72fa6539776178684b317bf61da6e4ac
borodin <vghost2005@mail.ru>
2024-05-03 11:16:00 +0700

    Dobavili chtenye iz file wrfile.txt

--- wrrdfile.c
+++ wrrdfile.c
@@ -7,6 +7,7 @@ int main() {
         int fd;
         size_t size;
         char string[] = "Gr. 3.124.2.23";
+        //Zapis v file
         if ((fd = open("wrfile.txt", O_WRONLY|O_CREAT, 0666))<0) {
                 printf("Ne udalos otryt file\n");
                 exit(-1); 
@@ -17,6 +18,23 @@ int main() {
                 exit(-1);
         }
         printf("Stroka zapisanav file wrfile.txt\n");
+        if (close(fd) <0) {
+                printf("Ne udalos zakryt file\n");
+                exit(-1);
+        }
+        //Chtenye iz file
+        char resstring[14];
+         if ((fd = open("wrfile.txt", O_RDONLY))<0) {
+                printf("Ne udalos otryt file\n");
+                exit(-1); 
+        }
+        size = read(fd ,resstring, 14);
+        if (size !=14) {
+                printf("Ne udalos prochitat iz file 14 byte\n");
+                exit(-1);
+        }
+        printf("Stroka prochitana file wrfile.txt\n");
+        printf("%s\n", resstring);
         if (close(fd) <0) {
                 printf("Ne udalos zakryt file\n");
                 exit(-1);
