0ff9a036c64ef7fb7034ead50ccc72212975afa3
borodin <vghost2005@mail.ru>
2024-05-31 11:30:00 +0700

    Dobavili v programmy, rabotayuchie s cemaforami, kommentarii

--- sems.c
+++ sems.c
@@ -5,22 +5,27 @@
 #include <stdlib.h>
 
 int main() {
-        int semid;
-        char pathname[] = "for_ipc.ipc";
-        key_t key;
-        struct sembuf mybuf;
+        int semid; //IPC-deskriptor dlya massiva cemafora
+        char pathname[] = "for_ipc.ipc"; //Imya file, s pomochyu kotorogo generiruetsa ipc-kluch 
+        key_t key;  //Peremennaya dlya hranenya ipc-klucha
+        struct sembuf mybuf;  //Structura dannyh, zadayuchaya operacii nad opredelennym cemaforom
+        //Process generacii ipc-klucha
         if((key = ftok(pathname, 0)) < 0) {
                 printf("Ne udalos sgenerirovat kluch ipc\n");
                 exit(-1);
         }
+        //Process poluchenya dostupa k massivu cemaforov dlya konkretnogo ipc-klucha
+        //Esli massiva cemaforov ne suchestvuet, to pytaemsya sozdat ego
         if((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
                 printf("Ne udalos poluchit  deskriptor massiva cemafora\n");
                 exit(-1);
         }
-        mybuf.sem_op = 1;
+        mybuf.sem_op = 1; //Kodirovanye operacii A(semidi,1)
         mybuf.sem_flg = 0;
         mybuf.sem_num = 0;
+        //Operacii budut osuchestvlyatsya nad cemaforom 0, kod operacii 1
         printf("Razblokirovka processa, zapushenogo ranee\n");
+        //Vypolnyaem operacii nad cemaforom
         if(semop(semid, &mybuf, 1) <0) {
                 printf("Ne udalos vypolnit operaciy nad cemaforom\n");
                 exit(-1);
